f220db364439f8452912a80522fcd1b9b4071d00
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2024-12-10 12:34:44 +0100

    feat: Add HTTP method validation

--- server.s
+++ server.s
@@ -1,15 +1,3 @@
-.section __TEXT,__text
-.globl _main
-.align 2
-.extern _printf
-
-// Constants
-.equ SOCK_STREAM, 1
-.equ AF_INET, 2
-.equ SOL_SOCKET, 0xffff
-.equ SO_REUSEADDR, 0x4
-.equ HTTP_PORT, 6969
-
 .section __TEXT,__const
 mom_response:
     .ascii "HTTP/1.1 413 Entity Too Large\r\n"
@@ -25,13 +13,17 @@ dad_response:
     .ascii "\r\n"
     .ascii "I'll buy some milk and get back soon"
 dad_response_len = . - dad_response
-// URL patterns to match
-urdad_url: .asciz "/urdad"
-urdad_url_len = . - urdad_url
+default_response:
+    .ascii "HTTP/1.1 405 Method Not Allowed\r\n"
+    .ascii "Content-Type: text/plain\r\n"
+    .ascii "Content-Length: 29\r\n"
+    .ascii "Allow: GET\r\n"
+    .ascii "\r\n"
+    .ascii "Method is not allowed for URL"
+default_response_len = . - default_response
+mom_url: .asciz "GET /urmom"
+dad_url: .asciz "GET /urdad"
 start_msg: .asciz "Server starting on port %d...\n"
-start_msg_len = . - start_msg
-socket_msg: .asciz "Socket created successfully\n"
-socket_msg_len = . - socket_msg
 listen_msg: .asciz "Server is listening...\n"
 listen_msg_len = . - listen_msg
 close_msg: .asciz "Connection closed\n"
@@ -50,9 +42,15 @@ _reuseaddr:
     .word 1
 
 .section __TEXT,__text
-// raw syscall print function (but check for the print of start message it uses libc printf)
-// it's literally just showing out that you can write EVERYTHING yourself
-print:
+.globl _main
+.align 2
+.equ SOCK_STREAM, 1
+.equ AF_INET, 2
+.equ SOL_SOCKET, 0xffff
+.equ SO_REUSEADDR, 0x4
+.equ HTTP_PORT, 6969
+
+print: // raw syscall print function 
     mov x2, x1 // x1 message length
     mov x1, x0 // x0 message address
     mov x0, #1 /// this is file descriptor 1 (stdout)
@@ -61,27 +59,19 @@ print:
     ret
 
 _main:
-    // Save frame pointer
-    stp x29, x30, [sp, #-16]!
-    mov x29, sp
-	
-    // use the libc printf to print the message
-    // I am not getting paid to manually convert ints to strings
     sub sp, sp, #16 // darwin passed printf values on stack o_O
     mov x8, HTTP_PORT
     str x8, [sp] 
     adrp x0, start_msg@PAGE
     add x0, x0, start_msg@PAGEOFF
-    bl _printf
+    bl _printf // libc call. I am not getting paid to manually convert ints to strings
     add sp, sp, #16 // increase stack back to normal but we don't use anyway
 
-    // Convert the port number to the host-to-network byte order
-    // and store it in the _sockaddr struct
-    mov w0, HTTP_PORT
-    bl _htons
     adrp x1, _sockaddr@PAGE
-    add x1, x1, _sockaddr@PAGEOFF
-    strh w0, [x1, #2]
+    add x1, x1, _sockaddr@PAGEOFF 
+    mov w0, HTTP_PORT
+    bl _htons // libc call again. Convert the port number to the host-to-network byte order
+    strh w0, [x1, #2] // and store it in the _sockaddr struct
 
     // In the code after socket creation and before bind
     // Create socket
@@ -95,22 +85,19 @@ _main:
     b.lt print_error
     mov x19, x0
 
-    // This is additional system wait for releasing the address before binding
-    mov x0, x19
-    mov x1, SOL_SOCKET
-    mov x2, SO_REUSEADDR
+    mov x1, SOL_SOCKET   // here we manually rebinding address we want to the new socket
+    mov x2, SO_REUSEADDR // without this it will work but you have to wait after each restart
     adrp x3, _reuseaddr@GOTPAGE
     ldr x3, [x3, _reuseaddr@GOTPAGEOFF]
     mov x4, #4
     mov x16, #105 // setsockopt syscall
     svc #0x80
 
-    // bind the osocket
     mov x0, x19
     adrp x1, _sockaddr@GOTPAGE
     ldr x1, [x1, _sockaddr@GOTPAGEOFF]
     mov x2, #16
-    mov x16, #104
+    mov x16, #104 // bind the osocket syscall
     svc #0x80
     
     cmp x0, #0 // Check error
@@ -121,17 +108,16 @@ _main:
     mov x1, #5
     mov x16, #106
     svc #0x80
-    // Check for listen error
-    cmp x0, #0
+
+    cmp x0, #0 // Check for listen error
     b.lt print_error
 
-    // Print listen success
-    adrp x0, listen_msg@PAGE
+    adrp x0, listen_msg@PAGE // Print listen success
     add x0, x0, listen_msg@PAGEOFF
     mov x1, listen_msg_len
     bl print
 
-request_loop: // Acceps the  connection and sends  the response
+request_loop: // Accepts the connection and sends the response
     mov x0, x19
     mov x1, #0
     mov x2, #0
@@ -141,9 +127,9 @@ request_loop: // Acceps the  connection and sends  the response
     cmp x0, #0 // Check accept error
     b.lt request_loop
     mov x20, x0 // Save client socket for future
-
     // Read request
     sub sp, sp, #1024 // Allocate buffer on stack
+    mov x0, x20
     mov x1, sp
     mov x2, #1024
     mov x16, #3 // read syscall
@@ -153,25 +139,37 @@ request_loop: // Acceps the  connection and sends  the response
     mov x0, sp // Buffer address
     bl print
 
-    // Find "/urdad" in the request using strstr
-    mov x0, sp 
-    adrp x1, urdad_url@PAGE
-    add x1, x1, urdad_url@PAGEOFF
+    mov x0, sp // Check for /urmom URL
+    adrp x1, mom_url@PAGE
+    add x1, x1, mom_url@PAGEOFF
     bl _strstr // yes again libc function but it's okay
+    cmp x0, #0
+    b.ne load_mom_response
+    mov x0, sp // Check for /urdad URL
 
+    adrp x1, dad_url@PAGE
+    add x1, x1, dad_url@PAGEOFF
+    bl _strstr
     cmp x0, #0
     b.ne load_dad_response
-    b load_mom_response
+    b load_default_response
 
 load_mom_response:
     adrp x21, mom_response@PAGE
     add x21, x21, mom_response@PAGEOFF
     mov x22, mom_response_len
     b send_response
+
 load_dad_response:
     adrp x21, dad_response@PAGE
     add x21, x21, dad_response@PAGEOFF
     mov x22, dad_response_len
+    b send_response
+
+load_default_response:
+    adrp x21, default_response@PAGE
+    add x21, x21, default_response@PAGEOFF
+    mov x22, default_response_len
     b send_response
 
 send_response:
@@ -189,6 +187,8 @@ send_response:
     add x0, x0, close_msg@PAGEOFF
     mov x1, close_msg_len
     bl print
+
+    add sp, sp, #1024
     b request_loop
 
 print_error:
